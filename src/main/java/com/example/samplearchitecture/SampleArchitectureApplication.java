6c5bb93affc491dc389ee5e9e58f49a7da7917dd
Francisco Serrano <francisco.serrano372@gmail.com>
2019-09-16 00:39:07 -0300

    Initial commit

---
+++ src/main/java/com/example/samplearchitecture/SampleArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplearchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleArchitectureApplication.class, args);
+    }
+
+}
