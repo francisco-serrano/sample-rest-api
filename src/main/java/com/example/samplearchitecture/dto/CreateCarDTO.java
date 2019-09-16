b8cdeff45857248eff862198392a6fc4af7fa8ba
Francisco Serrano <francisco.serrano372@gmail.com>
2019-09-16 01:45:11 -0300

    Adds custom naming for DTO fields

--- src/main/java/com/example/samplearchitecture/dto/CreateCarDTO.java
+++ src/main/java/com/example/samplearchitecture/dto/CreateCarDTO.java
@@ -1,49 +1,40 @@
 package com.example.samplearchitecture.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class CreateCarDTO {
+    @JsonProperty("brand")
     private String brand;
+
+    @JsonProperty("model")
     private String model;
+
+    @JsonProperty("version")
     private String version;
+
+    @JsonProperty("engine_size")
     private int engineSize;
+
+    @JsonProperty("horse_power")
     private int horsePower;
 
     public String getBrand() {
         return brand;
     }
 
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
     public String getModel() {
         return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
     }
 
     public String getVersion() {
         return version;
     }
 
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
     public int getEngineSize() {
         return engineSize;
     }
 
-    public void setEngineSize(int engineSize) {
-        this.engineSize = engineSize;
-    }
-
     public int getHorsePower() {
         return horsePower;
-    }
-
-    public void setHorsePower(int horsePower) {
-        this.horsePower = horsePower;
     }
 }
